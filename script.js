8fbca0710088895c78f6b6bef0616e99c5f90370
Vanshi1804 <92093339+Vanshi1804@users.noreply.github.com>
2023-12-14 16:27:53 +0530

    script.js

--- script.js
+++ script.js
@@ -1,78 +1,50 @@
-const cityInput = document.querySelector("#city-input");
-const searchButton = document.querySelector("#search-btn");
-const currentWeatherDiv = document.querySelector(".current-weather");
-const daysForecastDiv = document.querySelector(".days-forecast");
-const API_KEY = "c7a75c83acadf4f1c195ebc4fe4a3757"; // Paste your API here
-
-const createWeatherCard = (cityName, weatherItem, index) => {
-    if(index === 0) {
-        return `<div class="mt-3 d-flex justify-content-between">
-                    <div>
-                        <h3 class="fw-bold">${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h3>
-                        <h6 class="my-3 mt-3">Temperature: ${((weatherItem.main.temp - 273.15).toFixed(2))}°C</h6>
-                        <h6 class="my-3">Wind: ${weatherItem.wind.speed} M/S</h6>
-                        <h6 class="my-3">Humidity: ${weatherItem.main.humidity}%</h6>
-                    </div>
-                    <div class="text-center me-lg-5">
-                        <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather icon">
-                        <h6>${weatherItem.weather[0].description}</h6>
-                    </div>
-                </div>`;
-    } else {
-        return `<div class="col mb-3">
-                    <div class="card border-0 bg-secondary text-white">
-                        <div class="card-body p-3 text-white">
-                            <h5 class="card-title fw-semibold">(${weatherItem.dt_txt.split(" ")[0]})</h5>
-                            <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png" alt="weather icon">
-                            <h6 class="card-text my-3 mt-3">Temp: ${((weatherItem.main.temp - 273.15).toFixed(2))}°C</h6>
-                            <h6 class="card-text my-3">Wind: ${weatherItem.wind.speed} M/S</h6>
-                            <h6 class="card-text my-3">Humidity: ${weatherItem.main.humidity}%</h6>
-                        </div>
-                    </div>
-                </div>`;
+async function fetchCityAndWeather() {
+    try {
+        const { latitude , longitude } = await fetchCity();
+        await fetchWeather(latitude, longitude);
+    } catch (error) {
+        console.error('Error fetching data:', error);
     }
 }
 
-const getWeatherDetails = (cityName, latitude, longitude) => {
-    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
-    fetch(WEATHER_API_URL).then(response => response.json()).then(data => {
-        const forecastArray = data.list;
-        const uniqueForecastDays = new Set();
-        const fiveDaysForecast = forecastArray.filter(forecast => {
-            const forecastDate = new Date(forecast.dt_txt).getDate();
-            if (!uniqueForecastDays.has(forecastDate) && uniqueForecastDays.size < 6) {
-                uniqueForecastDays.add(forecastDate);
-                return true;
-            }
-            return false;
-        });
-        cityInput.value = "";
-        currentWeatherDiv.innerHTML = "";
-        daysForecastDiv.innerHTML = "";
-        fiveDaysForecast.forEach((weatherItem, index) => {
-            const html = createWeatherCard(cityName, weatherItem, index);
-            if (index === 0) {
-                currentWeatherDiv.insertAdjacentHTML("beforeend", html);
-            } else {
-                daysForecastDiv.insertAdjacentHTML("beforeend", html);
-            }
-        });        
-    }).catch(() => {
-        alert("An error occurred while fetching the weather forecast!");
+async function fetchCity() {
+    const cityApiKey = '57b4d3453841434d8d2dc11c482bfbdd';
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async position => {
+                const { latitude, longitude } = position.coords;
+
+                const reverseGeocodeApiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${cityApiKey}`;
+
+                try {
+                    const reverseGeocodeResponse = await fetch(reverseGeocodeApiUrl);
+                    const reverseGeocodeData = await reverseGeocodeResponse.json();
+                    const cityName = reverseGeocodeData.results[0].components.city;
+
+                    document.getElementById('city').textContent = cityName;
+                    resolve({ lat: latitude, lon: longitude });
+                } catch (error) {
+                    reject(error);
+                }
+            }, error => reject(error));
+        } else {
+            reject('Geolocation is not supported by your browser');
+        }
     });
 }
 
-const getCityCoordinates = () => {
-    const cityName = cityInput.value.trim();
-    if (cityName === "") return;
-    const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
-  
-    fetch(API_URL).then(response => response.json()).then(data => {
-        if (!data.length) return alert(`No coordinates found for ${cityName}`);
-        const { lat, lon, name } = data[0];
-        getWeatherDetails(name, lat, lon);
-    }).catch(() => {
-        alert("An error occurred while fetching the coordinates!");
-    });
+async function fetchWeather(lat, lon) {
+    const weatherApiKey = 'YOUR_API_KEY_HERE';
+    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
+
+    try {
+        const weatherResponse = await fetch(weatherApiUrl);
+        const weatherData = await weatherResponse.json();
+        const weatherInCelcius = (weatherData.main.temp - 273.15).toFixed(2);
+
+        document.getElementById('temperature').textContent = `${weatherInCelcius}°C`;
+        document.getElementById('condition').textContent = weatherData.weather[0].description;
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
 }
-searchButton.addEventListener("click", () => getCityCoordinates());
